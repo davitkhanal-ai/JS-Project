da0f46318f0af5ef9ee90d949684fcd01e5608f1
davitkhanal <khanaldavit@gmail.com>
2021-05-07 00:48:45 +0545

    added hue function

--- Fun with HTML5 Canvas/app.js
+++ Fun with HTML5 Canvas/app.js
@@ -6,14 +6,17 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADASS";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+ctx.lineWidth = 50;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e) {
-  if (!isDrawing) return; //stop the fucntion from running when mouse is not down
+  if (!isDrawing) return; //stop the function from running when mouse is not down
   console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   //start from 0
   ctx.moveTo(lastX, lastY);
@@ -21,6 +24,7 @@ function draw(e) {
   ctx.lineTo(e.offsetX, e.offsetY);
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
 }
 
 canvas.addEventListener("mousedown", (e) => {
