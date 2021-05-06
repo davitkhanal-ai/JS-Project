25a10d27fea07b62927307eac82727d504fc3856
davitkhanal <khanaldavit@gmail.com>
2021-05-07 00:40:56 +0545

    cnvs added with no contineu frm same line BW cllr

---
+++ Fun with HTML5 Canvas/app.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#BADASS";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+  if (!isDrawing) return; //stop the fucntion from running when mouse is not down
+  console.log(e);
+  ctx.beginPath();
+  //start from 0
+  ctx.moveTo(lastX, lastY);
+  //go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
